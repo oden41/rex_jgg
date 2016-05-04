50420ec9f2cbee612412dd9ad5d50c0112ebb9ad
oden <iamoden41@gmail.com>
2016-05-04 14:09:56 +0900

    JGG実装　総合テストの実装

---
+++ src/TJgg.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
+public class TJgg {
+	private Random fRandom;
+	private TPopulation fpopulation;
+	private TRex fRex;
+	private TSphereFunction fFunction;
+
+
+	public TJgg(TSphereFunction function,
+            TPopulation initialPopulation,
+            Random rand) {
+		fRandom = rand;
+		fpopulation = initialPopulation;
+		fFunction = function;
+
+		fRex = new TRex(rand);
+	}
+
+
+	public void doOneIteration() {
+		//複製選択 n+1個体ランダムに選択する．
+		int n = fpopulation.getIndividual(0).getVector().getDimension();
+		ArrayList<Integer> parentsIndex = new ArrayList<>();
+		TIndividual[] parents = new TIndividual[n + 1];
+		for (int i = 0; i < n + 1; i++) {
+			int index = fRandom.nextInt(fpopulation.getNumberOfPopulation());
+			while(parentsIndex.contains(index)){
+				index = fRandom.nextInt(fpopulation.getNumberOfPopulation());
+			}
+			parentsIndex.add(index);
+			parents[i] = fpopulation.getIndividual(index).clone();
+		}
+
+		//子個体の生成
+		TIndividual[] children = fRex.makeOffspring(parents, 5 * n);
+
+		//子個体の評価
+		for (int i = 0; i < children.length; i++) {
+			children[i].setEvaluationValue(fFunction.evaluate(children[i].getVector()));
+		}
+
+		//生存選択
+		//上位n+1個体をparentsIndexの個体と置き換える
+		Arrays.sort(children, (a,b) -> a.getEvaluationValue() > b.getEvaluationValue() ? 1 : (a.getEvaluationValue() < b.getEvaluationValue()) ? -1 : 0);
+		for (int i = 0; i < parentsIndex.size(); i++) {
+			fpopulation.getIndividual(parentsIndex.get(i)).copyFrom(children[i]);
+		}
+	}
+
+
+	public TPopulation getPopulation() {
+		return fpopulation;
+	}
+}
