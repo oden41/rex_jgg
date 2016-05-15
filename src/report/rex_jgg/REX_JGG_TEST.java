6bbcff0ef321cf172c292bef8ef992d6b79e30b8
oden <iamoden41@gmail.com>
2016-05-15 22:19:08 +0900

    GAViewer用にログを吐き出す用 ブランチ切り

--- src/report/rex_jgg/REX_JGG_TEST.java
+++ src/report/rex_jgg/REX_JGG_TEST.java
@@ -10,32 +10,36 @@ public class REX_JGG_TEST {
 
 	private static void executeOneTrial(TJgg jgg, PrintWriter pw, long maxEvals, int dimension, int noOfParents, int noOfKids) {
 		long noOfEvals = 0; // 評価回数を初期化．
+		long noOfGeneration = 0;
 		double best = jgg.getPopulation().getBestEvaluationValue(); // 集団の最良評価値を取得．
-		pw.println(noOfEvals + "," + best);// 初期集団の情報をログに保存．
+		jgg.d_Population.writeTo(pw);// 初期集団の情報をログに保存．
 		int loopCount = 0; // ループカウンタを初期化する．
 		while (best > 1e-7 && noOfEvals < maxEvals) { // 終了条件．最良値が10^-7以下，もしくは，評価回数が打ち切り評価回数を超えたとき．
 			loopCount++;
 			jgg.doOneIteration(); // GAの世代を１世代進める．
 			noOfEvals += noOfKids;
+			noOfGeneration++;
 			best = jgg.getPopulation().getBestEvaluationValue(); // 集団内の最良評価値を取得．
-			if (loopCount % 10 == 0) { // ループカウンタが１０の倍数のときにログをとる．
-				pw.println(noOfEvals + "," + best);
-				System.out.println("NoOfEvals:" + noOfEvals + ", Best:" + best); // 画面に試行数，評価回数，最良評価値を表示．
-			}
+//			if (loopCount % 10 == 0) { // ループカウンタが１０の倍数のときにログをとる．
+//				pw.println(noOfEvals + "," + best);
+//				System.out.println("NoOfEvals:" + noOfEvals + "NoOfBeneration:" + noOfGeneration +  ", Best:" + best); // 画面に試行数，評価回数，最良評価値を表示．
+//			}
+			pw.println(noOfEvals + "," + best);
+			System.out.println("NoOfEvals:" + noOfEvals + "NoOfBeneration:" + noOfGeneration +  ", Best:" + best); // 画面に試行数，評価回数，最良評価値を表示．
 		}
 		pw.println(noOfEvals + "," + best);// 最終世代のログをとる．
-		System.out.println("NoOfEvals:" + noOfEvals + ", Best:" + best); // 画面に試行数，評価回数，最良評価値を表示．
+		System.out.println("NoOfEvals:" + noOfEvals + "NoOfBeneration:" + noOfGeneration +  ", Best:" + best); // 画面に試行数，評価回数，最良評価値を表示．
 	}
 
 	public static void main(String[] args) throws IOException {
-		int dimension = 20; // 次元数
+		int dimension = 2; // 次元数
 		int populationSize = 7 * dimension; // 集団サイズ
 		int noOfParents = dimension + 1;
 		int noOfKids = 6 * dimension; // 子個体生成数
 		double min = -5.00; // 初期化領域の最小値
 		double max = +5.00; // 初期化領域の最大値
 		long maxEvals = (long) (4 * dimension * 1e4); // 打ち切り評価回数
-		String trialName = "RexJggSphereP14K5"; // 試行名
+		String trialName = "RexJggSphereTestLog"; // 試行名
 		String logFilename = trialName + ".csv"; // ログファイル名
 		PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(logFilename)));
 
--- src/report/rex_jgg/TJgg.java
+++ src/report/rex_jgg/TJgg.java
@@ -11,12 +11,21 @@ public class TJgg {
 	private int fNoOfParents;
 	private int fNoOfKids;
 
+	//以下デバッグ用変数
+	public TPopulation d_Population;
+	public TPopulation d_Parents;
+	public TPopulation d_Children;
+
 	public TJgg(TSphereFunction function, TPopulation initialPopulation, Random rand, int noOfParents, int noOfKids) {
 		fRandom = rand;
 		fpopulation = initialPopulation;
 		fFunction = function;
 		fNoOfParents = noOfParents;
 		fNoOfKids = noOfKids;
+
+		d_Population = new TPopulation();
+		d_Parents = new TPopulation();
+		d_Children = new TPopulation();
 
 		fRex = new TRex(fRandom);
 	}
@@ -25,6 +34,11 @@ public class TJgg {
 		// 複製選択
 		fpopulation.shuffle();
 		TIndividual[] parents = fpopulation.getRange(0, fNoOfParents);
+		d_Parents.setPopulationSize(parents.length);
+		for (int i = 0; i < parents.length; i++) {
+			d_Parents.getIndividual(i).copyFrom(parents[i]);
+		}
+
 
 		// 子個体の生成
 		TIndividual[] children = fRex.makeOffspring(parents, fNoOfKids);
@@ -32,6 +46,11 @@ public class TJgg {
 		// 子個体の評価
 		for (int i = 0; i < children.length; i++) {
 			children[i].setEvaluationValue(fFunction.evaluate(children[i].getVector()));
+		}
+
+		d_Children.setPopulationSize(children.length);
+		for (int i = 0; i < children.length; i++) {
+			d_Children.getIndividual(i).copyFrom(children[i]);
 		}
 
 		// 生存選択
